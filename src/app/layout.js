1e888fdfcb2ad841ccc92c8d25816a23c69ea0c9
Daniel V.O. Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-02-03 21:50:18 +0000

    main

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,13 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-<<<<<<< HEAD
-  title: 'Danielvor.tech',
-  description: 'Software Developer',
-=======
   title: 'Daniel Rodrigues - Software Engineer',
   description: 'Software Engineer based in São Paulo, Brasil. I build web applications with JavaScript, React, and Node.js. I also write about software development and career growth.',
->>>>>>> ad0963c (main)
 }
 
 export default function RootLayout({ children }) {
