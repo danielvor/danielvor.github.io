072ed3bb8f66bd13e76d0c41882a60a1144ea9f8
Daniel V.O. Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-02-03 16:27:10 +0000

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+//next.config.js
 
-module.exports = nextConfig
+module.exports = {
+    output: "export",
+    basePath: process.env.NODE_ENV === "production" ? "": undefined,
+    experimental: {
+      appDir: true,
+    },
+    images: {
+      unoptimized: true,
+    },
+    reactStrictMode: true,
+  }
