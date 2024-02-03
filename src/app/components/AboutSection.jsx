5a7b8dce90fa8ca0a3aadbff6ddea46907079cdb
Daniel V.O. Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-02-03 21:46:00 +0000

    main

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,12 +9,12 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
-        <li>Node.js</li>
         <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
-        <li>JavaScript</li>
-        <li>React</li>
+        <li>Next.js</li>
+        <li>Django</li>
+        <li>Flask</li>
+        <li>MySQL</li>
+        <li>MongoDB</li>
       </ul>
     ),
   },
@@ -23,8 +23,9 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+        <li>Fatec Itaquera</li>
+        <li>Etec Tiquatira</li>
+        <li>São Paulo Tech School</li>
       </ul>
     ),
   },
@@ -57,12 +58,13 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+          I'm a Software Developer, currently a student at Fatec Itaquera, studying algorithms, data analisys and programming.<br /> <br />
+
+            I am also on the technical course in Chemistry at ETEC Tiquatira. <br /> <br />
+
+            I work as Customer Experience at Banco Pan at Atento. <br /> <br />
+
+            Last but not least, student at 'ONE - Oracle Next Education' Oracle + Alura.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,14 +22,10 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
-                1000,
+                "Daniel Rodrigues",
+                2500,
                 "Web Developer",
-                1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
-                1000,
+                2500,
               ]}
               wrapper="span"
               speed={50}
@@ -37,8 +33,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+          Willingness to learn and rush to work!
           </p>
           <div>
             <Link
@@ -48,7 +43,8 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="/DanielVORodrigues.pdf"
+              target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
@@ -65,9 +61,9 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/danielvor-perfil.jpg"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
               width={300}
               height={300}
             />
