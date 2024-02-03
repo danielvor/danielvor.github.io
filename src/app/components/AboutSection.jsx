cf6bb1b5f55f3057886e0c7c704e498e60a0cd66
Daniel V.O. Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-02-03 22:19:41 +0000

    main

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -58,13 +58,13 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          I'm a Software Developer, currently a student at Fatec Itaquera, studying algorithms, data analisys and programming.<br /> <br />
+            I am a Software Developer, currently a student at Fatec Itaquera, studying algorithms, data analisys and programming.<br /> <br />
 
             I am also on the technical course in Chemistry at ETEC Tiquatira. <br /> <br />
 
             I work as Customer Experience at Banco Pan at Atento. <br /> <br />
 
-            Last but not least, student at 'ONE - Oracle Next Education' Oracle + Alura.
+            Last but not least, student at ONE - Oracle Next Education Oracle + Alura.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
